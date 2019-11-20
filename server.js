4ddd7c1a42c74abef2ed218d608cf8aefe97e309
IvayloSilovski1 <ivaylo.silovski689@gmail.com>
2019-11-20 21:20:12 +0100

    Initial setup

---
+++ server.js
@@ -0,0 +1,29 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const expressLayout = require('express-ejs-layouts');
+// const path = require('path');
+const indexRouter = require('./routes/index');
+
+
+// set up mongodb
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to MongoDB'));
+
+app.set('view engine', 'ejs');
+app.set('views',__dirname + 'views');
+app.set('layout', 'layouts/layout');
+
+app.use(expressLayout);
+app.use(express.static('public'));
+
+app.use('/', indexRouter);
+
+
+app.listen(process.env.PORT || 3000, () => console.log('Server started on port 3000'));
