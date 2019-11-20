322aa133dcec62e6f88670c470e22a8f0a493fb2
IvayloSilovski1 <ivaylo.silovski689@gmail.com>
2019-11-20 23:46:56 +0100

    Author databse

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 const expressLayout = require('express-ejs-layouts');
 // const path = require('path');
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+const bodyParser = require('body-parser');
 
 
 // set up mongodb
@@ -17,13 +19,15 @@ db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to MongoDB'));
 
 app.set('view engine', 'ejs');
-app.set('views',__dirname + 'views');
+app.set('views',__dirname + '/views');
 app.set('layout', 'layouts/layout');
+app.use(bodyParser.urlencoded({ limit:'10mb', extended: false}));
 
 app.use(expressLayout);
 app.use(express.static('public'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 
 app.listen(process.env.PORT || 3000, () => console.log('Server started on port 3000'));
